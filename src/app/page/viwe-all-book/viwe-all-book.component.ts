394e835ec0fb2b51e1544e716b3545037eb358fa
Janitha-viswagith <vishwagithjanith@gmail.com>
2024-02-08 20:33:03 +0530

    Book page done

--- src/app/page/viwe-all-book/viwe-all-book.component.ts
+++ src/app/page/viwe-all-book/viwe-all-book.component.ts
@@ -1,34 +1,50 @@
 import { CommonModule } from '@angular/common';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HttpClient,HttpClientModule  } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-viwe-all-book',
   standalone: true,
-  imports: [HttpClientModule,FormsModule,CommonModule],
+  imports: [HttpClientModule, FormsModule, CommonModule],
   templateUrl: './viwe-all-book.component.html',
-  styleUrl: './viwe-all-book.component.css'
-  
+  styleUrls: ['./viwe-all-book.component.css'] // Corrected styleUrl to styleUrls
 })
-export class ViweAllBookComponent implements OnInit{
-      private http;
-      public bookList:any ={};
+export class ViweAllBookComponent implements OnInit {
+  public bookList: any = {};
+  public setSelectedBook: any;
 
-      constructor(private httpCliant:HttpClient ){
-        this.http=httpCliant;
+  constructor(private httpClient: HttpClient) {}
 
+  ngOnInit(): void {
+    this.loadBook();
+  }
 
-      }
-       ngOnInit(): void {
-          this.lordBook();
-      }
-      lordBook(){
-       
-        this.http.get('http://localhost:8080/book/get').subscribe((data)=>{
-         this.bookList=data;  
-        console.log(this.bookList);
-      });
-      }
-      
+  loadBook(): void {
+    this.httpClient.get('http://localhost:8080/book/get').subscribe((data) => {
+      this.bookList = data;
+      console.log(this.bookList);
+    });
+  }
+
+  deleteBook(): void {
+    if (!this.setSelectedBook) {
+      console.error('No book selected to delete');
+      return;
     }
+
+    let api = `http://localhost:8080/book/${this.setSelectedBook.id}`;
+
+    this.httpClient.delete(api, { responseType: 'text' }).subscribe((response: string) => {
+      console.log(response);
+      this.loadBook();
+      this.setSelectedBook = null;
+    });
+  }
+
+  setSelectBook(book: any): void {
+    this.setSelectedBook = book;
+    console.log("setSelectBook" + book.id);
+    this.deleteBook();
+  }
+}
