418dc2a36690707997ae35b7b76f75fba4f3c1d0
Janitha-viswagith <vishwagithjanith@gmail.com>
2024-02-14 12:47:05 +0530

    add login and register pages

--- src/app/page/register/register.component.ts
+++ src/app/page/register/register.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient, HttpClientModule} from "@angular/common/http";
+import {FormsModule} from "@angular/forms";
+import {CommonModule} from "@angular/common";
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule,FormsModule,CommonModule ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
+  private http;
+  public countryList: any;
+  public selectedCountry : any;
 
+  constructor(private httpClient: HttpClient) {
+    this.http = httpClient;
+
+  };
+  ngOnInit(): void {
+
+    this.loadCountries();
+  }
+
+  loadCountries() {
+    this.http.get("https://restcountries.com/v3.1/all").subscribe((data: any) => {
+      console.log(data);
+      this.countryList = data;
+    });
+  }
+  SelectCountry(country: any) {
+   this.selectedCountry = country;
+    console.log(country);
+  }
 }
